b40bcee9aae8b58ece9829c5507a81ad97572009
Igor Braz <igor.braz@giran.com.br>
2014-05-30 17:36:34 -0300

    Testando Kruskal com o botão relatório
    Em modo debug insira o custo das arestas para testar

--- src/toggleButtonListeners/ContadorButton.java
+++ src/toggleButtonListeners/ContadorButton.java
@@ -1,19 +1,26 @@
 package toggleButtonListeners;
 
 import gui.AreaDesenho;
+import gui.KruskalAgmImpl;
 import gui.TelaPrincipal;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
 
+import basico.Aresta;
+import basico.Vertice;
+import desenho.Grafo;
+
 public class ContadorButton extends JButton implements ActionListener {
 	private JToolBar jToolBar;
 	private AreaDesenho areaDesenho;
+	private KruskalAgmImpl kr = new KruskalAgmImpl();
 	public ContadorButton(JToolBar toolBar_BarraPaint, AreaDesenho areaDesenho) {
 		setAreaDesenho(areaDesenho);
 		this.jToolBar = toolBar_BarraPaint;
@@ -34,6 +41,16 @@ public class ContadorButton extends JButton implements ActionListener {
 			int arestas = getAreaDesenho().getDraw().getListaArestas().size();
 			JOptionPane.showMessageDialog(null,"Total de vértices: "+vertices+
 											"\nTotal de arestas: "+arestas);
+			 List<Vertice> listaVertices = getAreaDesenho().getDraw().getListaVertices();
+			
+			 List<Aresta> listaArestas = getAreaDesenho().getDraw().getListaArestas();
+			 Grafo g = new Grafo();
+			 g.setListaArestas(listaArestas);
+			 g.setListaVertices(listaVertices);
+			 List<Aresta> listaNova = kr.getArvGeradoraMin(g);
+			 System.out.println(listaNova.size());
+			 
+			 
 		} else {
 			System.out.println("disable");
 		}
